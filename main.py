d4595ecda4cc3d0040b908a4b0eb6961ed18bbba
YC815 <minnecraft.YC815@gmail.com>
2023-06-04 20:26:33 +0800

    can't use

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 import os
 import sqlite3
 from discord.ext import commands
+import re
 
 load_dotenv()
 
@@ -22,18 +23,15 @@ cursor.execute('''
 ''')
 conn.commit()
 
-# 開機
 @bot.event
 async def on_ready():
     print(f">>> {bot.user} is ready <<<")
     await bot.change_presence(status=discord.Status.online, activity=discord.Game("錢！"))
 
-# Ping
 @bot.command(description="確認機器人在線狀況")
 async def ping(ctx):
     await ctx.respond("Pong! 我還在線喔！")
 
-# 說話賺錢
 @bot.event
 async def on_message(message):
     if not message.author.bot:
@@ -43,6 +41,7 @@ async def on_message(message):
         cursor.execute('SELECT balance FROM balances WHERE user_id=?', (author_id,))
         result = cursor.fetchone()
 
+        print(result)
         # 初始化使用者餘額
         if result is None:
             cursor.execute('INSERT INTO balances (user_id) VALUES (?)', (author_id,))
@@ -52,7 +51,7 @@ async def on_message(message):
             balance = result[0]
 
         # 更新使用者餘額
-        balance += 1
+        balance += 5
         cursor.execute('UPDATE balances SET balance=? WHERE user_id=?', (balance, author_id))
         conn.commit()
 
@@ -60,26 +59,38 @@ async def on_message(message):
         if balance >= 10:
             # 給予金錢
             await give_money(message.author, balance)
-
+            # print("+")
             # 重置使用者餘額
             cursor.execute('UPDATE balances SET balance=0 WHERE user_id=?', (author_id,))
             conn.commit()
 
-    await bot.process_commands(message)
+
+async def give_money(user, amount):
+    
+    channel = bot.get_channel(1114860808401342546)
+    money_amount = amount
+    await channel.send(f"{user} 獲得了 {money_amount} 元！")
+
+@bot.command()
+async def balance(ctx, user: discord.User = None):
+    if user is None:
+        user = ctx.author  # 若未提供 user 參數，預設為指令的執行者
+
+    user_id = str(user.id)
+    # 執行 SQL 查詢
+    cursor.execute("SELECT balance FROM balances WHERE user_id = ?", (user_id,))
+    result = cursor.fetchone()  # 獲取查詢結果的第一行資料
+
+    if result is None:
+        await ctx.send("找不到該使用者的餘額資料")
+    else:
+        balance = result[0]  # 從結果中取得餘額
+        await ctx.send(f"{user.mention} 的餘額是：{balance}")
 
 
-# # 給錢
-# @commands.has_permissions(administrator=True)
-# @bot.command(description="給使用者黏幣")
-# async def give_money(user, amount):
-#     print(user)
-#     cursor.execute('UPDATE balances SET balance=? WHERE user_id=?', (amount, user))
-#     conn.commit()
-#     await user.send(f"你獲得了 {amount} 元！")
 
-
+# ----------------------- #
+# 結尾
 bot.run(os.getenv('TOKEN'))
-
-# 關閉資料庫連線
 cursor.close()
 conn.close()
